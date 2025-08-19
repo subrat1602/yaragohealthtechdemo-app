2ffddf066764a831d77537cc330dda768255c40c
subrat1602 <subratswain.2000@gmail.com>
2025-08-19 18:48:00 +0530

    sub5

--- src/app/SubscriptionPlan/page.tsx
+++ src/app/SubscriptionPlan/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import PageBanner from "../../components/Common/PageBanner";
 import SubscriptionPlan from "../../components/Subscription/SubscriptionPlan";
-
 import CTA from "@/components/PCRepair/CTA";
 import Footer from "@/components/Fintech/Footer";
 import Navbar from "@/components/HealthTech/Navbar";
